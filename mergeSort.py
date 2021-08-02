188563d06d6d641b8d4e8c5300c1625b2ebfb041
Sand199 <83876822+Sand199@users.noreply.github.com>
2021-08-02 22:04:13 +0545

    Update mergeSort.py

--- mergeSort.py
+++ mergeSort.py
@@ -1,22 +1,29 @@
 def mergeSort(L):
+    """ Sorts a list at Complexity O(nlogn) where n is the len(L)"""
     def merge(La, Lb):
-        Ln = []  
+        Ln = []
         a = b = 0
-        while a < len(La) and b< len(Lb): 
-            if La[a] < Lb[b]: 
+
+        while a < len(La) and b < len(Lb):
+            if La[a] < Lb[b]:
                 Ln.append(La[a])
-                a += 1   
-            else:    
+                a += 1
+            else:
                 Ln.append(Lb[b])
-                b += 1   
-        if a == len(La): 
-            Ln.extend(Lb[b:])
-        else:    
-            Ln.extend(La[a:])
+                b += 1
+
+        while a < len(La):
+            Ln.append(L[a])
+            a += 1
+        while b < len(Lb):
+            Ln.append(L[b])
+            b += 1
         return Ln
 
     if len(L) < 2:
-        return L 
-    else:    
-        q = len(L)//2
-        return merge(mergeSort(L[0:q]),mergeSort(L[q:]))
+        return L
+
+    q = len(L)//2
+
+    return merge(mergeSort(L[0:q]),mergeSort(L[q:]))
+
